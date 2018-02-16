6764e1d474e6c7285a20cb012f3e934ae14ea309
APP236 <354185156@qq.com>
2018-02-16 14:00:18 +0800

    add

---
+++ CharacterMovement.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class CharacterMovement : MonoBehaviour
+{
+    public float Speed = 2.0F;
+
+
+
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+    }
+    void GetInput()
+    {
+        if ()
+        {
+
+        }
+        if (true)
+        {
+
+        }
+        if (true)
+        {
+
+        }
+        if (true)
+        {
+
+        }
+    }
+
+
+}
