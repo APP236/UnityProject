7fd31595e2d7abfec028bf425df5a8dfc7b1f019
APP236 <354185156@qq.com>
2018-02-16 22:15:34 +0800

    同步TouHou已有脚本

---
+++ SceneChange.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+
+public class SceneChange : MonoBehaviour
+{
+    string sceneName;
+    public void OnPress_Button()
+    {
+        if (EventSystem.current.IsPointerOverGameObject())
+        {
+            sceneName = EventSystem.current.currentSelectedGameObject.name;
+            if (sceneName == "Exit")
+            {
+                Application.Quit();
+            }
+            else
+            {
+                SceneManager.LoadScene(sceneName, LoadSceneMode.Single);
+            }
+
+        }
+
+    }
+}
